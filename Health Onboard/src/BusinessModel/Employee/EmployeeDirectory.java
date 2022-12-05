8a5b92d53f8ca3b0086d40c901825ef9cac7011c
Riya-Rajesh-Patil <patil.riy@northeastern.edu>
2022-12-05 15:54:46 -0500

    creating common business model

---
+++ Health Onboard/src/BusinessModel/Employee/EmployeeDirectory.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BusinessModel.Employee;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author patil
+ */
+public class EmployeeDirectory {
+    private final ArrayList<Employee> employeeList;
+
+    public EmployeeDirectory() {
+        employeeList = new ArrayList();
+    }
+
+    public ArrayList<Employee> getEmployeeList() {
+        return employeeList;
+    }
+
+    public Employee createEmployee(String employeeName) {
+        Employee newEmployee = new Employee();
+        newEmployee.setEmployeeName(employeeName);
+        employeeList.add(newEmployee);
+        return newEmployee;
+    }
+}
