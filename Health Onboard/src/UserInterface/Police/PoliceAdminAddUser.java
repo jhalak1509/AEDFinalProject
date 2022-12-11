b67abaf47ee9418240afae5e16162df8df2999cd
VinayKumarGudooru <gudooru.v@northeastern.edu>
2022-12-10 21:54:54 -0500

    UI Flow

--- Health Onboard/src/UserInterface/Police/PoliceAdminAddUser.java
+++ Health Onboard/src/UserInterface/Police/PoliceAdminAddUser.java
@@ -247,7 +247,7 @@ public class PoliceAdminAddUser extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-        new PoliceAdminAddUser().setVisible(false);
+        setVisible(false);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
